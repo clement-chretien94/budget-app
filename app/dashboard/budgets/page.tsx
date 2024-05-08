0ed440ab91bfcfec784692106db946b1c78695df
ClementC <clement.chretien94130@gmail.com>
2024-05-08 22:40:27 +0200

    Add budgets page and dashboard layout components

--- app/dashboard/budgets/page.tsx
+++ app/dashboard/budgets/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Budgets',
+};
+
 export default function Page() {
   return (
     <main>
       <h1 className="text-center text-xl font-bold">
-        Welcome to your Dashboard Page
+        Welcome to your Budgets Page
       </h1>
     </main>
   );
